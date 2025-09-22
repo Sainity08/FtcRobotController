da77659652938b7f7115ffbd2e85fb8766e6a26d
Sainity08 <sai08patel@gmail.com>
2025-09-21 23:53:04 -0500

    Update MecanumFieldOrientatedOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedOpMode.java
@@ -19,6 +19,6 @@ public class MecanumFieldOrientatedOpMode extends OpMode {
         x = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
 
-        drive.driveFieldRelative(y,x,turn);
+        drive.driveRobotRelative(y,x,turn);
     }
 }
