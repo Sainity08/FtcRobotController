d6b4a0bbb06968a74aff8b82a6e63e47a0cf739a
Sainity08 <sai08patel@gmail.com>
2025-09-21 23:16:16 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -42,7 +42,9 @@ public class MecanumDrive {
         double backRightPower = forward + strafe - rotate;
 
         double maxPower = 1.0;
-        double maxSpeed = 0.85;
+        double maxSpeed = 1.0;
+
+        //mecanum drive is goated
 
         maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
         maxPower = Math.max(maxPower, Math.abs(backLeftPower));
