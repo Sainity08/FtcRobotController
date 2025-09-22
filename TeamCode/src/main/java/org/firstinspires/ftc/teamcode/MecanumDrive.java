71dc4e2a76fe5639ee60df8fbb138c3c1c70b2e9
Sainity08 <sai08patel@gmail.com>
2025-09-21 23:35:21 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -35,36 +35,24 @@ public class MecanumDrive {
         imu.initialize(new IMU.Parameters(RevOrientation));
     }
 
-    public void drive(double forward, double strafe, double rotate) {
-        double frontLeftPower = forward + strafe + rotate;
-        double backLeftPower = forward - strafe + rotate;
-        double frontRightPower = forward - strafe - rotate;
-        double backRightPower = forward + strafe - rotate;
+    public void drive(double power, double theta, double turn) {
 
-        double maxPower = 1.0;
-        double maxSpeed = 1.0;
+        sin = Math.sin(theta - Math.PI/4);
+        cos = Math.cos(theta - Math.PI/4);
+        max = Math.max(Math.abs(sin),Math.abs(cos));
+        speed = 0.75;
+        newPower = power * speed;
 
-        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
-        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
-        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
-        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        leftFront = newPower * cos/max;
+        rightFront = newPower * sin/max;
+        leftRear = newPower * sin/max;
+        rightRear = newPower * cos/max;
 
-        frontLeftMotor.setPower(maxSpeed * (frontLeftPower/maxPower));
-        backLeftMotor.setPower(maxSpeed * (backLeftPower/maxPower));
-        frontRightMotor.setPower(maxSpeed * (frontRightPower/maxPower));
-        backRightMotor.setPower(maxSpeed * (backRightPower/maxPower));
-    }
-
-    public void driveFieldRelative(double forward, double strafe, double rotate) {
-        double theta = Math.atan2(forward, strafe);
-        double r = Math.hypot(strafe, forward);
-
-        theta = AngleUnit.normalizeRadians(theta -
-                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
-
-        double newForward = r * Math.sin(theta);
-        double newStrafe = r * Math.cos(theta);
-
-        this.drive(newForward, newStrafe, rotate);
+        If ((power + Math.abs(turn))>1){
+            leftFront /= newPower + Math.abs(turn);
+            rightFront /= newPower + Math.abs(turn);
+            leftRear /= newPower + Math.abs(turn);
+            rightRear /= newPower + Math.abs(turn);
+        }
     }
 }
