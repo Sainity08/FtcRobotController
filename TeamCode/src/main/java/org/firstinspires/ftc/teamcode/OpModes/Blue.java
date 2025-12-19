fda62aa9defbe61622d4b8fe4646d1b1d1db074e
Ryan Nguyen <75216497+Sainity08@users.noreply.github.com>
2025-12-19 15:44:19 -0600

    turret actually fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Blue.java
@@ -189,7 +189,7 @@ public class Blue extends OpMode {
         boolean hasTarget = (llResult.isValid());
         double axonPower = 0;
         if (autoAim && hasTarget) {
-            double kP = 0.0065;
+            double kP = 0.01;
             if (Math.abs(tx) < 1) {
                 axonPower = 0;
             } else {
